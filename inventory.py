aa149cab4071363afe34ad6d4a241ae1d9ef26cb
Angel <angelsolis2004@icloud.com>
2025-07-16 22:36:06 -0500

    get display name from each subkey

--- inventory.py
+++ inventory.py
@@ -7,12 +7,20 @@ registry = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
 RawKey = OpenKey(registry, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
 
 i = 0
+print("App Name : Subkey Name : Enum.")
 try:
     while True:
-        name = EnumKey(RawKey, i)
-        print(name, i)
+        subkey_name = EnumKey(RawKey, i)
+        subkey_path = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall" + '\\' + subkey_name
+        try:
+            subkey = OpenKey(registry, subkey_path)
+            display_name, reg_type = QueryValueEx(subkey, "DisplayName")
+            print(f"{display_name} : {subkey_name} : {i}")
+            CloseKey(subkey)
+        except FileNotFoundError: #for when subkey does not have DisplayName attribute
+            pass
         i = i + 1
 except WindowsError as e:
     print(f"Error: {e}" )
 
-print(RawKey)
+CloseKey(RawKey)
